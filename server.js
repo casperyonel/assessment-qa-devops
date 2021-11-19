87fe11a270ec7e11244e74ac593f07652ea6aeec
Casper Yonel <yonelcasper@gmail.com>
2021-11-19 10:40:16 -0700

    new

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const {shuffleArray} = require('./utils')
 
 app.use(express.json())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '60f486386df4493ab3d8536cf8bea61e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    // rollbar.info("HTML file served successfully")
+    rollbar.info("HTML file served successfully")
 })
 
 
