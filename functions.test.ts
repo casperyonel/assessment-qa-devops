f65f9929d126eef99f71b7d0c1be43674fbb29bb
Casper Yonel <yonelcasper@gmail.com>
2021-11-19 12:24:48 -0700

    final commit

--- functions.test.ts
+++ functions.test.ts
@@ -3,15 +3,12 @@ const {shuffleArray} = require('./utils')
 const {bots} = require('./data.js')
 
 describe('shuffleArray should', () => {
-    // test("Whether or not shuffleArray returns an array", () => {
-    //     let answerArray = shuffleArray(bots)
-    //     expect(typeof answerArray).toBe("array")
-    // })
-    test("Whether or not shuffleArray returns an array", () => {
-        let answerArray = shuffleArray(bots)
-        expect(bots).not.toBe(answerArray)
+    test("Testing to see if shuffleArray actually shuffled the array", () => {
+        let input = [1, 2, 3, 4, 5]
+        let answerArray = shuffleArray(input)
+        expect(answerArray.join('')).not.toBe(input.join(''))
     })
-    test("The length of the array", () => {
+    test("The length of the array thats returned is similar in length to input array", () => {
         let answerArray = shuffleArray(bots)
         expect(answerArray.length).toBe(bots.length)
     })
